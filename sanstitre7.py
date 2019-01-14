91a31148292c4fa81e8c84e37aee4ad2b59f2913
OnlyAtN1ght <simon.provot12@gmail.com>
2019-01-14 18:55:07 +0100

    v 1.0.0.2
    Code du 14/01 avec Mr Cucu et fichier du TD

--- sanstitre7.py
+++ sanstitre7.py
@@ -5,23 +5,101 @@ Created on Mon Jan  7 18:31:18 2019
 @author: provot
 """
 
-class Tableau:
+class Polynome:
     
     def __init__(self,tableau):
         self.tableau = tableau
         
         self.isNormalize = False
-        
+        self.isMonome = False
+
+        #Normalizing
         self.check()
+        if not self.isNormalize:
+            self.normalize()
+
+        #Check if monome
+        self.monome()
     
     def check(self):
-        if len(0):
+        #Check if the list is empty or full of zeros and then normalize it 
+
+        if len(self.tableau) == 0:
             self.isNormalize = True
-        if list(l) == [0] * len(l):
+        if self.tableau == [0] * len(self.tableau):
             self.tableau = []
             self.isNormalize = True
         
+
+    def monome(self):
+        number_of_zero = 0
+        for i in self.tableau:
+            if i == 0:
+                number_of_zero += 1
+        #If true it's a monome
+        if number_of_zero == len(self.tableau)-1:
+            self.isMonome = True
+
+
+    def __add__(self,other):
+        #Check type
+        if type(other) != type(self):
+            print("ERROR")
+            return 0
+
+        result = []
+        if len(self.tableau) < len(other.tableau):
+            self.tableau += [0]*(len(other.tableau)-len(self.tableau))
+        if len(self.tableau) > len(other.tableau):
+            other.tableau += [0]*(len(self.tableau)-len(other.tableau))
+
+        for i in range(len(self.tableau)):
+            result.append(self.tableau[i] + other.tableau[i])
+        return Polynome(result)
+    
+    def __mul__(self, other):
+        #Multiply polynome
+     
+        #Check type
+        if type(other) != type(self):
+            print("ERROR")
+            return 0
         
+        result = []
+
+        #Make the tableaux the same lenght
+        if len(self.tableau) < len(other.tableau):
+            self.tableau += [0]*(len(other.tableau)-len(self.tableau))
+        if len(self.tableau) > len(other.tableau):
+            other.tableau += [0]*(len(self.tableau)-len(other.tableau))
+
+        #If other or self is monome #FAUX
+        if other.isMonome or self.isMonome:
+            for i in range(len(self.tableau)):
+                pass
+
+            self.normalize()
+            other.normalize()
+            return Polynome(result)
+
+        #If none are monome
+        #Make the other polynome many monome
+        #Prob just
+        monomes = []
+        for i in range(len(other.tableau)):
+            s = ([0] * i) + [other.tableau[i]]
+            monomes.append(Polynome(s))
+        pres = []
+        for monome in monomes:
+            k = self * monome
+            pres.append(k)
+        result = Polynome([])
+        for element in pres:
+            result += element
+        return result
+
+
+      
     def normalize(self):
         if self.isNormalize:
             return self.tableau
@@ -39,27 +117,14 @@ class Tableau:
         if j==-1:
             return ([])
         
-        
-        
-""""  
-        
- #Check if the list is empty
-    if len(T) == 0:
-        return ("",T)
-    
-        
-    
-    
-    s = ""
-    for i in range(len(T)):
-        s += "{}x^{} + ".format(T[i],i)
-    s = s[:-2])
-    return (s,T)"""
-    
 
 
 if __name__=="__main__":
     t = [2,5,5,1,65,0,0,0]
-    nigga = Tableau(t)
-    nigga.normalize()
-    print(nigga.tableau)
+    m = [0,0,0,0,0,1,0,0,0]
+    test = [1,1,1]
+    tab = Polynome(m)
+    tab2 = Polynome(test)
+    print(tab2.tableau)
+    f = tab2*tab2
+    print(f.tableau)
